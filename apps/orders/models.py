bfda1b6953e9d637ccc9830a4b4d701d7dcc2f11
issmushi <issmushi@gmail.com>
2026-03-27 14:03:02 +0300

    add PromoCode Order and OrderItem models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from apps.products.models import Goods
+from apps.promos.models import PromoCode
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='orders',
+    )
+
+    promo_code = models.ForeignKey(
+        PromoCode,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name='orders',
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.pk}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='items',
+    )
+    goods = models.ForeignKey(
+        Goods,
+        on_delete=models.PROTECT,
+        related_name='order_items'
+    )
+
+    quantity = models.PositiveIntegerField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return f"{self.goods} x {self.quantity}"
