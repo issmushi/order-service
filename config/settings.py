dfd00ab2fed1da7047dc7c75babf9906bfe95439
issmushi <issmushi@gmail.com>
2026-03-27 12:08:20 +0300

    add apps (orders, products, promos) to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.products',
+    'apps.orders',
+    'apps.promos',
 ]
 
 MIDDLEWARE = [
