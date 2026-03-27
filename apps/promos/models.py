0ac7fc01e7ba6996c4587a4fd8adc126e7db4f61
issmushi <issmushi@gmail.com>
2026-03-27 13:48:17 +0300

    add PromoUsage model

--- apps/promos/models.py
+++ apps/promos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 from apps.products.models import Category
 
 
@@ -6,7 +8,7 @@ class PromoCode(models.Model):
     code = models.CharField(max_length=50, unique=True)
 
     discount_percent = models.DecimalField(
-        max_digits=5,
+        max_digits=4,
         decimal_places=2
     )
 
@@ -22,3 +24,30 @@ class PromoCode(models.Model):
 
     def __str__(self):
         return self.code
+
+
+class PromoUsage(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='promo_usages'
+    )
+
+    promo_code = models.ForeignKey(
+        PromoCode,
+        on_delete=models.CASCADE,
+        related_name='usages'
+    )
+
+    used_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'promo_code'],
+                name='unique_user_promo_code'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} - {self.promo_code}"
