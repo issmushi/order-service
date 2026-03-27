c6f5be6deb4a7edf0ad23ec4af52f48752e82381
issmushi <issmushi@gmail.com>
2026-03-27 13:30:40 +0300

    rename Product model to Goods

--- apps/products/models.py
+++ apps/products/models.py
@@ -8,17 +8,21 @@ class Category(models.Model):
         return self.name
 
 
-class Product(models.Model):
+class Goods(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     category = models.ForeignKey(
         Category,
         on_delete=models.PROTECT,
-        related_name='products'
+        related_name='goods'
     )
 
     is_excluded_from_promos = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+
+
+
