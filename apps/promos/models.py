c26f5dcadc7bb924cfd146b8f07491bc805b10f9
issmushi <issmushi@gmail.com>
2026-03-27 13:39:40 +0300

    add PromoCode model

--- apps/promos/models.py
+++ apps/promos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from apps.products.models import Category
 
-# Create your models here.
+
+class PromoCode(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+
+    discount_percent = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
+
+    expires_at = models.DateTimeField()
+
+    max_usages = models.PositiveIntegerField()
+
+    categories = models.ManyToManyField(
+        Category,
+        blank=True,
+        related_name='promo_codes'
+    )
+
+    def __str__(self):
+        return self.code
